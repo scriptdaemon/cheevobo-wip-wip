172884ab4aa5ff55c9c99b7b3a072c21039ebc94
Kenneth Williams <scriptdaemon@users.noreply.github.com>
2017-02-02 10:11:04 -0800

    feat(bot): Connect bot to Steam network (#6)

---
+++ bin/cheevobot.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// -- Dependencies -------------------------------------------------------------
+
+// Local modules
+const SteamBot = require('../lib/steam-bot')
+
+// -- Entry Point --------------------------------------------------------------
+
+const cheevobot = new SteamBot()
+cheevobot.start(err => {
+  if (err) throw err
+  console.log('connected')
+})
