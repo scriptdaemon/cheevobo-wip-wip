d85d7e23b83d66d9a365036af36aac20f3ad637d
Kenneth Williams <scriptdaemon@gmail.com>
2017-02-13 16:22:26 -0800

    feat(bot): Add whitelist/blacklist support
    Add support for a whitelist and blacklist that can be used to allow
    or disallow certain users from accessing certain commands or other
    sensitive features.

--- test/lib/steam-bot.test.js
+++ test/lib/steam-bot.test.js
@@ -19,22 +19,22 @@ const SteamBot = proxyquire('../../lib/steam-bot', { 'steam-user': SteamUser })
 // -- Tests --------------------------------------------------------------------
 
 tap.test('SteamBot', tap => {
-  tap.plan(3)
+  tap.plan(7)
 
   //
   // Methods
   //
 
-  tap.test('#start should not pass an error', tap => {
-    const bot = new SteamBot(path.resolve('../fixtures/config.json'))
+  tap.test('#start should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
     bot.start(err => {
       tap.error(err)
       tap.end()
     })
   })
 
-  tap.test('#stop should not pass an error', tap => {
-    const bot = new SteamBot(path.resolve('../fixtures/config.json'))
+  tap.test('#stop should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
     bot.start(err => {
       tap.error(err)
       bot.stop(err => {
@@ -44,11 +44,55 @@ tap.test('SteamBot', tap => {
     })
   })
 
-  tap.test('#exec should not pass an error', tap => {
-    const bot = new SteamBot(path.resolve('../fixtures/config.json'))
+  tap.test('#exec should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
     bot.start(err => {
       tap.error(err)
-      bot.exec('--help', (err, argv, output) => {
+      bot.exec('cmd', (err, argv, output) => {
+        tap.error(err)
+        tap.end()
+      })
+    })
+  })
+
+  tap.test('#whitelist should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
+    bot.start(err => {
+      tap.error(err)
+      bot.whitelist('ID', err => {
+        tap.error(err)
+        tap.end()
+      })
+    })
+  })
+
+  tap.test('#unwhitelist should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
+    bot.start(err => {
+      tap.error(err)
+      bot.unwhitelist('ID', err => {
+        tap.error(err)
+        tap.end()
+      })
+    })
+  })
+
+  tap.test('#blacklist should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
+    bot.start(err => {
+      tap.error(err)
+      bot.blacklist('ID', err => {
+        tap.error(err)
+        tap.end()
+      })
+    })
+  })
+
+  tap.test('#unblacklist should not error', tap => {
+    const bot = new SteamBot(path.join(__dirname, '../fixtures/config.json'))
+    bot.start(err => {
+      tap.error(err)
+      bot.unblacklist('ID', err => {
         tap.error(err)
         tap.end()
       })
